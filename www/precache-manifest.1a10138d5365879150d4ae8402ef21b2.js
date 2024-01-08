72b931180cf3f9011784aaf47709295d9af14136
AnushikaNethsara <anushikanethsara@gmail.com>
2024-01-08 16:15:29 +0530

    push

---
+++ www/precache-manifest.1a10138d5365879150d4ae8402ef21b2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2df3f08b38d81b3909e5986c5049863",
+    "url": "./index.html"
+  },
+  {
+    "revision": "453ae3828823e5593b0c",
+    "url": "./static/css/main.4b387738.chunk.css"
+  },
+  {
+    "revision": "57eac7b23b3f35e71a81",
+    "url": "./static/js/2.a3eaf640.chunk.js"
+  },
+  {
+    "revision": "cb16adfedd99bb6ffb611dffed33c8a6",
+    "url": "./static/js/2.a3eaf640.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "453ae3828823e5593b0c",
+    "url": "./static/js/main.1b9d952a.chunk.js"
+  },
+  {
+    "revision": "2e6a252cb1bcf926f9c9",
+    "url": "./static/js/runtime-main.b9041976.js"
+  },
+  {
+    "revision": "0db3043f181b8cf82eff078086a21fbf",
+    "url": "./static/media/WebXpay-Logo.0db3043f.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
